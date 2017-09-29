7f9373375fe6edea3c928dd45b7eb68254c1de79
Kseniya <kostrova.kseniya@yandex.ru>
2017-09-29 17:16:39 +0300

    client

--- store-server/src/main/java/com/kostrova/store/GoodsController.java
+++ store-server/src/main/java/com/kostrova/store/GoodsController.java
@@ -3,7 +3,9 @@ package com.kostrova.store;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +20,7 @@ public class GoodsController {
 	private GoodService goodService;
 
 	@RequestMapping(method = RequestMethod.POST, path = "/good", consumes = {MediaType.APPLICATION_JSON_VALUE})
-	public void addNewGood(@RequestBody Good good) throws WrongPropertyValueException {
+	public Good addNewGood(@RequestBody Good good) throws WrongPropertyValueException {
 		if (good.getId() <= 0) {
 			throw new WrongPropertyValueException("Impossible value of id");
 		} else if (good.getQuantity() < 0) {
@@ -27,15 +29,17 @@ public class GoodsController {
 			throw new WrongPropertyValueException("Impossible value of price");
 		}
 		goodService.addNewGood(good);
+		return good;
 	}
 
 	
 	@RequestMapping(method = RequestMethod.PUT, path = "/good")
-	public void addGood(@RequestBody Good good) throws WrongPropertyValueException {
+	public Good addGood(@RequestBody Good good) throws WrongPropertyValueException {
 		if (good.getQuantity() < 0) {
 			throw new WrongPropertyValueException("Impossible value of quantity");
 		}
 		goodService.addExistingGood(good);
+		return good;
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, path = "/good/{goodId}")
@@ -48,16 +52,18 @@ public class GoodsController {
 
 	@Secured({"ROLE_MANAGER", "ROLE_EMPLOYEE"})
 	@RequestMapping(method = RequestMethod.GET, path = "/good/{goodId}")
-	public Good getGoodInfo(@PathVariable("goodId") Integer goodId) throws WrongPropertyValueException {
+	public ResponseEntity<Good> getGoodInfo(@PathVariable("goodId") Integer goodId) throws WrongPropertyValueException {
 		if (goodId <= 0) {
 			throw new WrongPropertyValueException("Impossible value of id");
 		}
-		return goodService.getGood(goodId);
+		return new ResponseEntity<Good>(goodService.getGood(goodId), HttpStatus.OK);
+	//	return goodService.getGood(goodId);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/goods", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	public List<Good> getInfoAboutAllGoods() {
-		return goodService.getAllGoods();
+	public ResponseEntity<List<Good>> getInfoAboutAllGoods() {
+		return new ResponseEntity<List<Good>>(goodService.getAllGoods(), HttpStatus.OK);
+	//	return goodService.getAllGoods();
 	}
 
 }
